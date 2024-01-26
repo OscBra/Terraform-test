d620027c397346d226902b335f70975646112a44
OscBra <oscarbrasoe@outlook.com>
2024-01-26 12:20:50 +0100

    Virtual network

--- main.tf
+++ main.tf
@@ -11,12 +11,14 @@ provider "azurerm" {
 features {}
 }
 
-resource "azurerm_resource_group" "first" {
-    name = "learn-tf-rg-norwayeast"
-    location = "norwayeast"
+resource "azurerm_resource_group" "main" {
+  name = "learn-tf-rg-norwayeast"
+  location = "norwayeast"
 }
 
-resource "azurerm_resource_group" "second" {
-    name = "learn-tf-rg-norwayeast2"
-    location = "norwayeast"
+resource "azurerm_virtual_network" "main" {
+  name = "learn-tf-vnet-norwayeast"
+  location = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space = ["10.0.0.0/16"]
 }
